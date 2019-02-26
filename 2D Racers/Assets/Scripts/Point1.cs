9fb0e5a7a1db13d11a73f908f6c283a955590647
Unknown <jacobmiller9991@gmail.com>
2019-02-26 10:49:23 -0500

    Pushable

---
+++ 2D Racers/Assets/Scripts/Point1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.SetPositionAndRotation(new Vector3(450, 0, 0), new Quaternion());
+    }
+
+}
