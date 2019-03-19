e8b294b63036d24d58155308a717e386b5e34c98
Justin <jfirsching2@gmail.com>
2019-03-18 21:05:03 -0400

    Add Camera Controller + Reorganize
    Add camera controller attempt.  Organize models and scenes

---
+++ 2D Racers/Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    private Camera cam;
+
+    public Vector3 offset = Vector3.zero;
+
+    public Vector3 currentPos = new Vector3(0f, 10f, 3f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void LateUpdate(){
+        LookAtTarget();
+        MoveToTarget();
+    }
+
+    void LookAtTarget()
+    {
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 2.5f * Time.deltaTime);
+    }
+
+    void MoveToTarget(){
+        Vector3 pos = target.position + 
+                      target.forward * offset.z +
+                      target.right * offset.x + 
+                      target.up * offset.y;
+        transform.position = Vector3.Lerp(transform.position, pos, 2.5f * Time.deltaTime);
+    }
+}
--- 2D Racers/Assets/Scripts/CarController.cs
+++ 2D Racers/Assets/Scripts/CarController.cs
@@ -20,59 +20,54 @@ public class CarController : MonoBehaviour
 
     public Rigidbody car_rb;
 
-
-    Quaternion targetRotation;
-
     // Start is called before the first frame update
     void Start()
     {
         car_rb = GetComponent<Rigidbody>();
-        car_rb.centerOfMass += new Vector3(0f, 0f, 1f);
     }
 
-    private void Update()
+    private void AntiRoll(Axle axle)
     {
+        // Handle AntiRoll
+        bool leftHit, rightHit;
+        float leftTravel = 1f, rightTravel = 1f;
+        float antirollForce;
+        WheelHit hit;
+        leftHit = axle.leftWheel.GetGroundHit(out hit);
+        if(leftHit){
+            leftTravel = (-axle.leftWheel.transform.InverseTransformPoint(hit.point).y - axle.leftWheel.radius) / axle.leftWheel.suspensionDistance;
+        }
+        rightHit = axle.rightWheel.GetGroundHit(out hit);
+        if(rightHit){
+            rightTravel = (-axle.rightWheel.transform.InverseTransformPoint(hit.point).y - axle.rightWheel.radius) / axle.rightWheel.suspensionDistance;
+        }
+        antirollForce = (leftTravel - rightTravel) * axle.antirollStiffness;
 
+        if(leftHit){
+            car_rb.AddForceAtPosition(transform.up * -antirollForce, axle.leftWheel.transform.position);
+        }
+        if(rightHit){
+            car_rb.AddForceAtPosition(transform.up * antirollForce, axle.rightWheel.transform.position);
+        }
     }
 
     void FixedUpdate()
     {
         float vert = Input.GetAxis("Vertical");
         float horiz = Input.GetAxis("Horizontal");
-        float leftTravel = 1f, rightTravel = 1f, antirollForce;
-        WheelHit hit;
-        bool leftHit, rightHit;
+
         foreach(Axle axle in axles){
             // Handle Steering
             if (axle.steers){
                 axle.leftWheel.steerAngle = horiz * maxRotationAngle;
                 axle.rightWheel.steerAngle = horiz * maxRotationAngle;
             }
-
             // Handle Acceleration
             if (axle.powered){
                 axle.leftWheel.motorTorque = vert * maxTorque;
                 axle.rightWheel.motorTorque = vert * maxTorque;
-                print(vert * maxTorque);
             }
-
-            // Handle AntiRoll
-            leftHit = axle.leftWheel.GetGroundHit(out hit);
-            if(leftHit){
-                leftTravel = (-axle.leftWheel.transform.InverseTransformPoint(hit.point).y - axle.leftWheel.radius) / axle.leftWheel.suspensionDistance;
-            }
-            rightHit = axle.rightWheel.GetGroundHit(out hit);
-            if(rightHit){
-                rightTravel = (-axle.rightWheel.transform.InverseTransformPoint(hit.point).y - axle.rightWheel.radius) / axle.rightWheel.suspensionDistance;
-            }
-            antirollForce = (leftTravel - rightTravel) * axle.antirollStiffness;
-
-            if(leftHit){
-                car_rb.AddForceAtPosition(transform.up * -antirollForce, axle.leftWheel.transform.position);
-            }
-            if(rightHit){
-                car_rb.AddForceAtPosition(transform.up * antirollForce, axle.rightWheel.transform.position);
-            }
+            AntiRoll(axle);
         }
     }
 }
